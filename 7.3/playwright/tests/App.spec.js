482ea9117e384b2bcb0ebae4933d29901a668af8
InnaB <isbocha@gmail.com>
2023-12-07 23:28:50 +0300

    Add 7.3/playwright/tests/App.spec.js

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,32 @@
 const { test, expect } = require("@playwright/test");
+const user = require('../user');
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
-
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
-
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
-
-  page.click("text=Бизнес и управление");
-
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-  );
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://netology.ru/?modal=sign_in');
 });
+
+test("successful authorization", async ({ page }) => {
+  await page.click('[placeholder="Email"]');
+  await page.fill('[placeholder="Email"]', user(validLogin));
+  await page.click('[placeholder="Пароль"]');
+  await page.fill('[placeholder="Пароль"]', user(validPassword));
+  await page.click('[data-testid="login-submit-btn"]');
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'https://netology.ru/profile' }*/),
+    page.click('[data-testid="login-submit-btn"]')
+     ]);
+  await expect(
+    page.getByRole('[data-testid="profile-programs-content"] >> text=Моё обучение')
+  ).toBeVisible({ timeout: 10000 });
+ });
+
+ test("failed authorization", async ({ page }) => {
+  await page.click('[placeholder="Email"]');
+  await page.fill('[placeholder="Email"]', user(invalidLogin));
+  await page.click('[placeholder="Пароль"]');
+  await page.fill('[placeholder="Пароль"]', user(invalidPassword));
+  await page.click('[data-testid="login-submit-btn"]');
+  await expect(
+    page.getByRole('[data-testid="login-error-hint"] >> text=Вы ввели неправильно логин или пароль')
+  ).toBeVisible({ timeout: 10000 });
+  });

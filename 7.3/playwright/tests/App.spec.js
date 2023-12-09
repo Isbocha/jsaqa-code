f3bc46e3917b217495d84e6f28a38efcc749fb2e
InnaB <isbocha@gmail.com>
2023-12-09 14:29:39 +0300

    Add new 7.3/playwright/tests/App.spec.js

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -5,28 +5,26 @@ test.beforeEach(async ({ page }) => {
   await page.goto('https://netology.ru/?modal=sign_in');
 });
 
+
 test("successful authorization", async ({ page }) => {
   await page.click('[placeholder="Email"]');
-  await page.fill('[placeholder="Email"]', user(validLogin));
+  await page.fill('[placeholder="Email"]', user.validLogin);
   await page.click('[placeholder="Пароль"]');
-  await page.fill('[placeholder="Пароль"]', user(validPassword));
+  await page.fill('[placeholder="Пароль"]', user.invalidPassword);
   await page.click('[data-testid="login-submit-btn"]');
-  await Promise.all([
-    page.waitForNavigation(/*{ url: 'https://netology.ru/profile' }*/),
-    page.click('[data-testid="login-submit-btn"]')
-     ]);
-  await expect(
-    page.getByRole('[data-testid="profile-programs-content"] >> text=Моё обучение')
-  ).toBeVisible({ timeout: 10000 });
- });
+  await expect(page.locator('[data-testid="profile-programs-content"] >> text=Моё обучение').isVisible);
+  await page.screenshot({ path: './screenshots/Success.png' });
+  });
+
 
  test("failed authorization", async ({ page }) => {
   await page.click('[placeholder="Email"]');
-  await page.fill('[placeholder="Email"]', user(invalidLogin));
+  await page.fill('[placeholder="Email"]', user.invalidLogin);
   await page.click('[placeholder="Пароль"]');
-  await page.fill('[placeholder="Пароль"]', user(invalidPassword));
+  await page.fill('[placeholder="Пароль"]', user.invalidPassword);
   await page.click('[data-testid="login-submit-btn"]');
-  await expect(
-    page.getByRole('[data-testid="login-error-hint"] >> text=Вы ввели неправильно логин или пароль')
-  ).toBeVisible({ timeout: 10000 });
+  await expect(page.locator('Вы ввели неправильно логин или пароль').isVisible);
+  await page.screenshot({ path: './screenshots/Fail.png' });
   });
+
+
